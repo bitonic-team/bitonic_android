ddd7f8db66f320dab5912d211979f0e4a536007c
Stephane Truong <horumena@gmail.com>
2014-12-05 01:29:31 +0100

    httpClient (try)
    httpClient (try)

--- app/src/main/java/fr/intechinfo/bitonic/MainActivity.java
+++ app/src/main/java/fr/intechinfo/bitonic/MainActivity.java
@@ -1,37 +1,50 @@
 package fr.intechinfo.bitonic;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
-import fr.intechinfo.bitonic.model.Place;
 
 
 public class MainActivity extends Activity {
 
     private GoogleMap mMap;
     List<Marker> markers = new ArrayList<Marker>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +53,33 @@ public class MainActivity extends Activity {
         //Google Maps
         setContentView(R.layout.googlemaps);
 
-
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
+        //geolocalisation
         /*
         mMap.setMyLocationEnabled(true);
 
         CurrentLocationProvider currentLP = new CurrentLocationProvider(this);
         mMap.setLocationSource(currentLP);*/
+
+        //it works
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                HttpClient client = new DefaultHttpClient();
+
+                HttpGet request = new HttpGet("http://178.62.27.132/api/places/");
+                String response = "";
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                try {
+                    response = client.execute(request, handler);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Log.i("pas d'erreur",response);
+            }
+        };
+        thread.start();
 
         Marker marker = mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(17.644022, -0.791016))
@@ -64,26 +96,6 @@ public class MainActivity extends Activity {
 
         markers.add(marker);
         //.icon(BitmapDescriptorFactory.fromResource(R.drawable.car)));
-    }
-
-    private void InitializePlaces(String service) throws IOException {
-        HttpGet httpget = new HttpGet(service);
-        ResponseHandler<String> responseHandler = new BasicResponseHandler();
-
-        String s = new DefaultHttpClient().execute(httpget, responseHandler);
-
-        try {
-            JSONObject jObj = new JSONObject(s);
-
-            Place p = new Place();
-            p.Id = jObj.getInt("_id");
-            p.Title = jObj.getString("name");
-          
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
     }
 
     @Override
